37aae303cedd11a2011f40baee0f20db5b1ec8b0
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-28 00:36:47 -0400

    add LoginView :heavy_plus_sign:

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("account/update/", update_account),
-    path("login/", login),
+    path("login/", csrf_exempt(LoginView.as_view())),
     path("signup/", signup),
-    path("store/<int:id>", get_store_by_id),
+    path("store/<int:id>", csrf_exempt(GetStoreByIDView.as_view())),
     path("store/top/", top_stores),
     path("app-reviews/", get_app_reviews),
     path("shipping-info/", get_addresse),
--- user/views.py
+++ user/views.py
@@ -1,28 +1,52 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+import json
 
 
 def update_account(request):
     return
 
 
-def login(request):
-    return
+class LoginView(APIView):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            response_data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
+            return Response(response_data)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 def signup(request):
     return
 
-def get_store_by_id(request, id):
-    return
+
+class GetStoreByIDView(APIView):
+    def get(self, request, id):
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 def top_stores(request):
     return
 
+
 def get_app_reviews(request):
     return
 
+
 def get_addresse(requesr):
-    return 
+    return
+
 
 def create_addresse(request):
     return
