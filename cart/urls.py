652aa4321bb5ca4b2f44e9f5eaa4fbb01ae36d3d
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-30 11:56:39 -0400

    fix urls :lady_beetle:

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import * 
+from .views import *
 
-urlpatterns = [
-    path("cart/<int:id>/products/", get_cart_details),
-    path("cart/<int:id>/add-product/", add_product_to_cart),
-    path("cart/<int:id>/delet-product/", delete_product_from_cart),
-
+urlpatterns = [  # ver
+    path("cart/products/", get_cart_details),
+    path("cart/add-product/", add_product_to_cart),
+    path("cart/delet-product/", delete_product_from_cart),
+    path("cart/update-product/", update_product_in_cart),
 ]
