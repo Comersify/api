863af9ad02d574e8d02e65d35bdbf679e633ab80
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-30 11:56:58 -0400

    fix urls :lady_beetle:

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path("wish-list/<int:id>/products/", get_wish_list_details),
-    path("wish-list/<int:id>/add-product/", add_product_to_wish_list),
-    path("wish-list/<int:id>/delet-product/", delete_product_from_wish_list),
+    path("wish-list/products/", get_wish_list_details),
+    path("wish-list/add-product/", add_product_to_wish_list),
+    path("wish-list/delete-product/", delete_product_from_wish_list),
 ]
