a9e11dfc6b75602f2bdca618cf5e0ab0935ac5f1
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-01 06:51:47 -0400

    update urls :dart:

--- product/urls.py
+++ product/urls.py
@@ -8,16 +8,16 @@ urlpatterns = [
     path("products/create/", create_product),
     path("products/delete/<int:id>/", delete_product),
     path("products/update/<int:id>/", update_product),
-    path("products/top-deals/", top_deals),
+    path("products/super-deals/", top_deals), # ver
     path("products/<int:id>/", get_product_details),
 
-    path("categories/top/", top_categories),
-    path("categories/", get_categories),
+    path("categories/top/", top_categories), # ver
+    path("categories/", get_categories), # ver
 
-    path("product/<int:id>/reviews", get_reviews),
+    path("product/<int:id>/reviews", get_reviews), # ver
 
-    path("coupons/", get_coupon),
-    path("coupons/<int:id>", get_coupon_by_id),
+    path("my-coupons/", get_coupon),
+    path("coupon/<str:code>", get_coupon_by_code), # ver
     path("coupons/create/", create_coupon),
     path("coupons/delete/<int:id>/", delete_coupon),
     path("coupons/update/<int:id>/", update_coupon),
