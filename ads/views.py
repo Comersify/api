dc5abf2ee039ff7c784ad35042e3b24f016bcc56
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-01 16:38:58 -0400

    create get ads view :heavy_check_mark:

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import AdsSerializer
+from .models import Ads
 
-# Create your views here.
-def get_ads(request):
-    return
+
+class GetAdsView(APIView):
+    def get(self, request):
+        query = Ads.objects.all()
+        serializer = AdsSerializer(query, many=True)
+        return Response({"type": "success", "data": serializer.data})
