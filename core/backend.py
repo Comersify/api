b3425374a5452704d3257c23124a0ecf8b6c3cf5
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-03 17:20:47 -0400

    fix auth returns :lady_beetle:

--- core/backend.py
+++ core/backend.py
@@ -8,12 +8,11 @@ User = get_user_model()
 class AccessTokenBackend(JWTAuthentication):
     def authenticate(self, request):
         try:
-            decoded_token = self.get_validated_token(
-                request.headers['Authorization'])
+            token = request.headers['Authorization']
+            decoded_token = self.get_validated_token(token)
             user = User.objects.filter(id=decoded_token['user_id']).get()
             request.user = user
-
         except InvalidToken:
             return None
 
-        return (user, None)
+        return (user, token)
