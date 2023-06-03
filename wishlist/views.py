3fdea003ae8f880b9de782cb885955b48b1cbabd
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-03 17:14:51 -0400

    create logic of delete & add product :heavy_check_sign:

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,17 +1,69 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import WishList
+from product.models import Product
+from rest_framework.permissions import IsAuthenticated
+from core.backend import AccessTokenBackend
 
 
 class GetWishListDetailsView(APIView):
-    def get(request):
+    def get(self, request):
         return Response({"type": "error", "message": "Not developed yet"})
+
+
+class ProductInWishListView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
+    def get(self, request, id):
+        wish_list = WishList.objects.filter(user_id=request.user.id)
+        if wish_list.exists():
+            wish_list = wish_list.get()
+            data = wish_list.products.filter(id=id).exists()
+            return Response({"type": "success", "data": data})
+        return Response({"type": "success", "data": False})
 
 
 class AddProductToWishListView(APIView):
-    def post(request):
-        return Response({"type": "error", "message": "Not developed yet"})
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
+    def post(self, request):
+        wish_list = WishList.objects.filter(user_id=request.user.id)
+        product = Product.objects.filter(id=request.data['product_id'])
+        if not product.exists():
+            return Response({"type": "error", "message": "Product doesn't exist"})
+        product = product.get()
+        if wish_list.exists():
+            in_wish_list = wish_list.filter(
+                products__id=request.data['product_id'])
+            if len(in_wish_list) > 0:
+                return Response({"type": "error", "message": "Product already in your list"})
+            else:
+                wish_list = wish_list.get()
+                wish_list.products.add(product)
+                return Response({"type": "success", "message": "Product added to your list"})
+        else:
+            wish_list = WishList.objects.create(
+                user_id=request.user.id,
+            )
+            wish_list.products.add(product)
+            return Response({"type": "success", "message": "Product added to your list"})
 
 
 class DeleteProductFromWishList(APIView):
-    def post(request):
-        return Response({"type": "error", "message": "Not developed yet"})
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
+    def post(self, request):
+        wish_list = WishList.objects.filter(user_id=request.user.id)
+        product = Product.objects.filter(id=request.data['product_id'])
+        if not product.exists():
+            return Response({"type": "error", "message": "Product doesn't exist"})
+        product = product.get()
+        if wish_list.exists():
+            wish_list = wish_list.get()
+            wish_list.products.remove(product)
+            return Response({"type": "success", "message": "Product removed from your list"})
+        else:
+            return Response({"type": "success", "message": "Product is not in your list"})
