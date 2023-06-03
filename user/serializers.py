30a58c54966ab1e5e9c208c2fbee438483b516b2
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-03 17:17:16 -0400

    add missing data :heavy_plus_sign:

--- user/serializers.py
+++ user/serializers.py
@@ -1,31 +1,36 @@
 from .models import Store, AppReviews
-from django.db.models import Count, Avg
+from django.db.models import Count, Avg, Value
 from product.serializers import ProductSerializer
 from rest_framework import serializers
+from django.db.models.functions import Coalesce
 
 
 class StoreSerializer:
     def get_top_stores(self):
         stores = Store.objects.annotate(order_count=Count(
-            'product__order'), reviews_avg=Avg('product__review')).order_by('-order_count', "-reviews_avg").values('name', 'logo', 'description', 'reviews_avg')[:8]
+            'product__order'), reviews_avg=Avg('product__review')).order_by('-order_count', "-reviews_avg").values('id', 'name', 'logo', 'description', 'reviews_avg')[:8]
         return list(stores)
 
     def get_store_details(self, id):
-        store = Store.objects.filter(id=id).annotate(
+        store = Store.objects.filter(pk=id).annotate(
             orders=Count('product__order'),
             reviews=Count('product__review'),
-            reviews_avg=Avg('product__review')
+            reviews_avg=Coalesce(Avg('product__review'), Value(0.0))
         )
         if not store.exists():
             return False
+        store = store.get()
+        products = ProductSerializer().get_products_by_store_id(store_id=id)
         data = {
+            "id": store.id,
             "cover": store.cover.url,
             "logo": store.logo.url,
             "name": store.name,
             "description": store.description,
             "orders": store.orders,
             "reviews": store.reviews,
-            "products": ProductSerializer().get_data(store_id=id)
+            "reviews_avg": store.reviews_avg,
+            "products": products
         }
         return data
 
