0c375e8de49835eb1308d3904de99b8616c936e7
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-03 17:15:56 -0400

    add has product url :heavy_plus_sign:

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("wish-list/products/", GetWishListDetailsView.as_view()),
+    path("wish-list/has-product/<int:id>", ProductInWishListView.as_view()),
     path("wish-list/add-product/", AddProductToWishListView.as_view()),
     path("wish-list/delete-product/", DeleteProductFromWishList.as_view()),
 ]
