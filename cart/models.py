ddbfda2b1f2e5a5b72171831e77070b7816ad9de
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-04 16:35:42 -0400

    delete unused choices :heavy_minus_sign:

--- cart/models.py
+++ cart/models.py
@@ -7,11 +7,6 @@ USER = get_user_model()
 
 class ShoppingCart(models.Model):
 
-    class StatusChoices(models.TextChoices):
-
-        SUBMITTED = "SUBMITTED", "SUBMITTED"
-        IN_PROGRESSE = "IN_PROGRESSE", "IN_PROGRESSE"
-
     user = models.ForeignKey(USER, on_delete=models.SET_NULL, null=True,  limit_choices_to={
         'user_type': 'CUSTOMER'})
     orders = models.ManyToManyField("order.Order",  limit_choices_to={
