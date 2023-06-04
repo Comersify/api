c866a251dc372abcf582818fbcb2bdd607520799
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-04 16:36:36 -0400

    add apply coupon url :heavy_plus_sign:

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [  # ver
     path("cart/products/", GetCartDetailsView.as_view()),
     path("cart/add-product/", AddProoductToCartView.as_view()),
-    path("cart/delet-product/", DeleteProoductFromCartView.as_view()),
+    path("cart/delete-product/", DeleteProoductFromCartView.as_view()),
     path("cart/update-product/", UpdateProoductInCartView.as_view()),
+    path("cart/apply-coupon/", ApplyCouponView.as_view()),
 ]
