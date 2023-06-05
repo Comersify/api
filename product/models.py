89ce4271c13ceccd301b461ba3df1da948919922
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-05 15:00:11 -0400

    add clean function :heavy_plus_sign:

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 import os
 
 USER = get_user_model()
@@ -68,18 +69,22 @@ class Review(models.Model):
 class Coupon(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     code = models.CharField(max_length=20)
-    percentage = models.IntegerField(validators=[
-        MinValueValidator(0, message='Minimum value is 1.'),
-        MaxValueValidator(90, message='Maximum value is 100.')
+    value = models.FloatField(validators=[
+        MinValueValidator(1, message='Minimum value is 1.'),
     ])
     end_date = models.DateTimeField(auto_now=True, auto_now_add=False)
     start_date = models.DateTimeField(auto_now_add=True)
+
+    def clean(self) -> None:
+        if self.product and self.product.price < self.value:
+            return ValidationError("Value is not valid")
+        return super().clean()
 
 
 class Discount(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
-    percentage = models.IntegerField(validators=[
+    percentage = models.PositiveIntegerField(validators=[
         MinValueValidator(0, message='Minimum value is 1.'),
         MaxValueValidator(90, message='Maximum value is 100.')
     ])
