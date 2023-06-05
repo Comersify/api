98eb16af55ecbf93ef0f883c000c8046227eb045
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-05 14:59:49 -0400

    add clean function :heavy_plus_sign:

--- order/models.py
+++ order/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.db.models import Q
+from django.core.exceptions import ValidationError
 from datetime import date
 USER = get_user_model()
 
@@ -13,18 +13,24 @@ class Order(models.Model):
         SHIPPED = 'SHIPPED', "SHIPPED"
         DELEVRED = 'DELEVRED', "DELEVRED"
 
-    user = models.ForeignKey(USER, on_delete=models.CASCADE, limit_choices_to={
+    user = models.ForeignKey(USER, on_delete=models.SET_NULL, null=True, limit_choices_to={
         'user_type': 'CUSTOMER'})
     product = models.ForeignKey(
         "product.Product", on_delete=models.SET_NULL, null=True)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
     pack = models.ForeignKey('product.ProductPackage',
-                             on_delete=models.CASCADE)
+                             on_delete=models.SET_NULL, null=True, blank=True)
     status = models.CharField(
         choices=StatusChoices.choices, default=StatusChoices.IN_CART, max_length=10)
-
     coupon = models.ForeignKey(
         "product.Coupon", on_delete=models.SET_NULL, null=True, blank=True,
-        limit_choices_to=Q(product=models.F('product'), end_date__gt=date.today()))
-
+        limit_choices_to={"product": models.F('product'), "end_date__lt": date.today()})
+    price = models.FloatField(null=True, blank=True)
     created_at = models.DateTimeField(auto_created=True, auto_now_add=True)
+
+    def clean(self) -> None:
+        if self.product.id and self.pack.product.id != self.product.id:
+            raise ValidationError("Pack is not valid")
+        if self.product.id and self.coupon.product.id != self.product.id:
+            raise ValidationError("Coupon is not valid")
+        return super().clean()
