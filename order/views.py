004405686b5f6ed251b39f02085cb9761ab7a2d8
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-06 15:49:02 -0400

    add create order and get order logic ;heavy_plus_sign:

--- order/views.py
+++ order/views.py
@@ -1,12 +1,88 @@
 from rest_framework.response import Response
+from product.models import Discount
+from datetime import date
 from rest_framework.views import APIView
+from user.models import ShippingInfo
+from cart.models import ShoppingCart
+from rest_framework.permissions import IsAuthenticated
+from core.backend import AccessTokenBackend
+from .models import Order
 
 
 class GetMyOrdersView(APIView):
-    def get(request):
-        return Response({"type": "error", "message": "not developed yet"})
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
+    def get(self, request):
+        try:
+            orders = Order.objects.filter(user_id=request.user.id).exclude(status='IN_CART').values(
+                'id', 'product__title', 'quantity', 'price', 'status', 'created_at')
+            data = list(orders)
+            return Response({"type": "success", "data": data})
+        except:
+            return Response({'type': 'error', 'message': 'Something went wrong try later'})
 
 
 class CreateOrderView(APIView):
-    def get(request):
-        return Response({"type": "error", "message": "not developed yet"})
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
+    def post(self, request):
+        try:
+            address = request.data.get('address')
+            phone_number = request.data.get('phoneNumber')
+            postal_code = request.data.get('postalCode')
+
+            if not address or not phone_number or not postal_code:
+                return Response({"type": "error", "message": "Please enter all needed informations"})
+
+            info = ShippingInfo.objects.filter(
+                user_id=request.user.id,
+                address=address,
+                phone_number=phone_number,
+                postal_code=postal_code
+            )
+            shipping_info_id = None
+            if not info.exists():
+                info = ShippingInfo.objects.create(
+                    user_id=request.user.id,
+                    address=address,
+                    phone_number=phone_number,
+                    postal_code=postal_code
+                )
+                shipping_info_id = info.id
+            else:
+                info = info.get()
+                shipping_info_id = info.id
+
+            cart = ShoppingCart.objects.filter(
+                user_id=request.user.id
+            )
+            if not cart.exists():
+                return Response({'type': 'error', 'message': 'No orders found'})
+            cart = cart.get()
+            orders = cart.orders.all()
+            if len(orders) <= 0:
+                return Response({'type': 'error', 'message': 'No orders found'})
+
+            for order in orders:
+                order.status = "SUBMITTED"
+                order.shipping_info_id = shipping_info_id
+                price = order.product.price
+                discount = Discount.objects.filter(
+                    product_id=order.product.id, end_date__lt=date.today())
+                if discount.exists():
+                    discount = discount.get()
+                    price -= (price * discount.percentage / 100)
+                price = price * order.quantity
+                if order.coupon:
+                    price -= order.coupon.value
+                order.price = price
+                from datetime import datetime
+                order.created_at = datetime.now()
+                order.save()
+            cart.orders.clear()
+            cart.save()
+            return Response({'type': 'success', 'message': 'Your was submitted wait the seller to shipe it'})
+        except:
+            return Response({'type': 'error', 'message': 'Something went wrong try later'})
