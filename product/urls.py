310c39e0742c0f5b6d8d3e157158a7e24152d906
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-08 15:00:14 -0400

    reduce urls :recycle:

--- product/urls.py
+++ product/urls.py
@@ -5,25 +5,18 @@ urlpatterns = [
 
     path("products/", GetProductsView.as_view()),
     path("my-products/", GetMyProductsView.as_view()),
-    path("products/create/", CreateProductView.as_view()),
-    path("products/delete/<int:id>/", DeleteProductView.as_view()),
-    path("products/update/<int:id>/", UpdateProductView.as_view()),
     path("products/super-deals/", GetSuperDealsView.as_view()),  # ver
     path("products/id/<int:id>/", GetProductDetailsView.as_view()),
 
     path("categories/top/", GetHotCategoriesView.as_view()),  # ver
     path("categories/", GetCategoriesView.as_view()),  # ver
 
+    path("coupon/<str:code>", GetCouponByCodeView.as_view()),  # ver
     path("reviews/<int:id>", GetReviewsView.as_view()),  # ver
 
-    path("my-coupons/", GetCouponView.as_view()),
-    path("coupon/<str:code>", GetCouponByCodeView.as_view()),  # ver
-    path("coupons/create/", CreateCouponView.as_view()),
-    path("coupons/delete/<int:id>/", DeleteCouponView.as_view()),
-    path("coupons/update/<int:id>/", UpdateCouponView.as_view()),
 
-    path("discounts/", GetDiscountView.as_view()),
-    path("discounts/create/", CreateDiscountView.as_view()),
-    path("discounts/delete/<int:id>/", DeleteCouponView.as_view()),
-    path("discounts/update/<int:id>/", UpdateProductView.as_view()),
+    path("vendor/products/", ProductView.as_view()),
+    path("vendor/coupons/", CouponView.as_view()),
+    path("vendor/discounts/", DiscountView.as_view()),
+
 ]
