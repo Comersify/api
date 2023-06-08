b5019188dfd8e054b26579165bc6470b6b296452
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-08 16:54:02 -0400

    fix stars & orders count :lady_beetle:

--- user/serializers.py
+++ user/serializers.py
@@ -1,21 +1,28 @@
 from .models import Store, AppReviews
-from django.db.models import Count, Avg, Value
+from django.db.models import Count, Avg, Value, Subquery, OuterRef
 from product.serializers import ProductSerializer
 from rest_framework import serializers
 from django.db.models.functions import Coalesce
+from order.models import Order
 
 
 class StoreSerializer:
     def get_top_stores(self):
-        stores = Store.objects.annotate(order_count=Count(
-            'product__order'), reviews_avg=Avg('product__review')).order_by('-order_count', "-reviews_avg").values('id', 'name', 'logo', 'description', 'reviews_avg')[:8]
+        subquery_order = Order.objects.filter(status="DELEVRED").filter(id=OuterRef('id')).annotate(orders_count=Count('id')).values('orders_count')[:1]
+        stores = Store.objects.annotate(order_count=Subquery(subquery_order), reviews_avg=Coalesce(Avg('product__review__stars'), Value(0.0))
+            ).order_by('-order_count', "-reviews_avg"
+            ).values(
+                'id', 'name', 'logo', 'order_count',
+                'description', 'reviews_avg'
+            )[:8]
         return list(stores)
 
     def get_store_details(self, id):
+        subquery_order = Order.objects.filter(product__store__id=OuterRef('id'), status="DELEVRED").annotate(orders_count=Count('id')).values('orders_count')[:1]
         store = Store.objects.filter(pk=id).annotate(
-            orders=Count('product__order'),
+            orders=Coalesce(Subquery(subquery_order),0),
             reviews=Count('product__review'),
-            reviews_avg=Coalesce(Avg('product__review'), Value(0.0))
+            reviews_avg=Coalesce(Avg('product__review__stars'), Value(0.0))
         )
         if not store.exists():
             return False
