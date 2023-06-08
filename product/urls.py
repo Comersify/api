de2e612bb02aaacae088f4f04fc26f11d853dce0
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-08 16:53:10 -0400

    delete unused urls :heavy_minus_sign:

--- product/urls.py
+++ product/urls.py
@@ -4,14 +4,12 @@ from .views import *
 urlpatterns = [
 
     path("products/", GetProductsView.as_view()),
-    path("my-products/", GetMyProductsView.as_view()),
     path("products/super-deals/", GetSuperDealsView.as_view()),  # ver
     path("products/id/<int:id>/", GetProductDetailsView.as_view()),
 
     path("categories/top/", GetHotCategoriesView.as_view()),  # ver
     path("categories/", GetCategoriesView.as_view()),  # ver
 
-    path("coupon/<str:code>", GetCouponByCodeView.as_view()),  # ver
     path("reviews/<int:id>", GetReviewsView.as_view()),  # ver
 
 
