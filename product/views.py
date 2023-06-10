dda3acf6317490427bd7a344aa756cf4791bf93f
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-10 16:01:46 -0400

    add discount & coupon views :heavy_plus_sign:

--- product/views.py
+++ product/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import ProductSerializer, CategorySerializer, ReviewsSerializer
+from .serializers import ProductSerializer, CategorySerializer, ReviewsSerializer, CouponSerializer, DiscountSerializer
 from django.db.models import Q
-
-
+from .models import Coupon , Discount
+from rest_framework.permissions import IsAuthenticated
+from core.backend import AccessTokenBackend
+    
 class GetSuperDealsView(APIView):
     def get(self, request):
         try:
@@ -65,7 +67,12 @@ class GetProductsView(APIView):
 
 
 class ProductView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
+
     def get(self, request):
+        
         return Response({"type": "error", "data": "not developed yet "})
 
     def post(self, request):
@@ -80,33 +87,138 @@ class ProductView(APIView):
 
 
 class CouponView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
     def get(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        if request.user.user_type == "VENDOR":
+            serializer = CouponSerializer()
+            data = serializer.get_data(request.user.id)
+            return Response({"type": "success", "data": list(data)})
+        return Response({"type": "error", "message": "There is no coupons for you"})
+
 
     def post(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        if request.user.user_type == "VENDOR":
+            product_id = request.data.get('product_id')
+            code = request.data.get('code')
+            value = request.data.get('value')
+            end_date = request.data.get('end_date')
+            if not product_id or not code or not value or not end_date:
+                return Response({"type": "error", "message": "complete missing data"})
+            coupon = Coupon.objects.create(
+                product= product_id,
+                code= code,
+                value= value,
+                end_date= end_date,
+            )
+            return Response({"type": "success", "message": "coupon created successfully"})
+        return Response({"type": "error", "message": "There is no coupons for you"})
+
 
     def delete(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        if request.user.user_type == "VENDOR":
+            coupon_id = request.data.get("id")
+            if not coupon_id:
+                return Response({"type": "error", "message": "can't delete coupon"})
+            coupon = Coupon.objects.filter(id=coupon_id, product__store__user__id=request.user.id)
+            if not coupon.exists():
+                return Response({"type": "error", "data": "Coupon not found"})
+            coupon = coupon.get()
+            coupon.delete() 
+            return Response({"type": "success", "message": "coupon was deleted"})
+        return Response({"type": "error", "message": "There is no coupons for you"})
+
 
     def update(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        if request.user.user_type == "VENDOR":
+            coupon_id =  request.data.get('id')
+            product_id = request.data.get('product_id')
+            code = request.data.get('code')
+            value = request.data.get('value')
+            end_date = request.data.get('end_date')
+            if not coupon_id or not product_id or not code or not value or not end_date:
+                return Response({"type": "error", "message": "complete missing data"})
+
+            coupon = Coupon.objects.filter(id=coupon_id, product__store__user__id=request.user.id)
+            if not coupon.exists():
+                return Response({"type": "error", "data": "Coupon not found"})
+            coupon = coupon.get()
+            coupon.product_id = product_id 
+            coupon.code = code 
+            coupon.value = value 
+            coupon.end_date = end_date 
+            coupon.save()
+            return Response({"type": "success", "message": "coupon updated successfully" })
+        return Response({"type": "error", "message": "There is no coupons for you"})
+            
 
 
 
 
 class DiscountView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [AccessTokenBackend]
+
+
     def get(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        serializer = DiscountSerializer()
+        data = serializer.get_data()  
+        return Response({"type": "success", "data": list(data)})
 
     def post(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        if request.user.user_type == "VENDOR":
+            product_id = request.data.get('product_id')
+            title = request.data.get('title')
+            percentage = request.data.get('percentage')
+            end_date = request.data.get('end_date')
+            if not product_id or not title or not percentage or not end_date:
+                return Response({"type": "error", "message": "complete missing data"})
+            discount = Discount.objects.create(
+                product= product_id,
+                title= title,
+                percentage= percentage,
+                end_date= end_date,
+            )
+            return Response({"type": "success", "message": "discount created successfully", "data": discount})
+        return Response({"type": "error", "message": "There is no discounts for you"})
+
 
     def delete(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        if request.user.user_type == "VENDOR":
+            discount_id = request.data.get("id")
+            if not discount_id:
+                return Response({"type": "error", "message": "can't delete discount"})
+            discount = Discount.objects.filter(id=discount_id, product__store__user__id=request.user.id)
+            if not discount.exists():
+                return Response({"type": "error", "data": "discount not found"})
+            discount = discount.get()
+            discount.delete() 
+            return Response({"type": "success", "message": "discount was deleted"})
+        return Response({"type": "error", "message": "There is no discounts for you"})
+
 
     def update(self, request):
-        return Response({"type": "error", "data": "not developed yet "})
+        if request.user.user_type == "VENDOR":
+            discount_id =  request.data.get('id')
+            product_id = request.data.get('product_id')
+            title = request.data.get('title')
+            percentage = request.data.get('percentage')
+            end_date = request.data.get('end_date')
+            if not discount_id or not product_id or not title or not percentage or not end_date:
+                return Response({"type": "error", "message": "complete missing data"})
+
+            discount = Discount.objects.filter(id=discount_id, product__store__user__id=request.user.id)
+            if not discount.exists():
+                return Response({"type": "error", "data": "discount not found"})
+            discount = discount.get()
+            discount.product_id = product_id 
+            discount.title = title 
+            discount.percentage = percentage 
+            discount.end_date = end_date 
+            discount.save()
+            return Response({"type": "success", "message": "discount updated successfully" })
+        return Response({"type": "error", "message": "There is no discounts for you"})
 
 
 
