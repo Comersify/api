614dec8a45831cc84517195fc5e84e4188272f0e
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-11 13:13:04 -0400

    add get customers serializer :heavy_plus_sign:

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("account/info/", SettingsView.as_view()),
+    path("vendor/customers/", GetCustomersView.as_view()),
     path("account/update/", UpdateSettingsView.as_view()),  # ver
     path("login/", LoginView.as_view()),  # ver
     path("refresh/", RefreshTokenView.as_view()),  # ver
