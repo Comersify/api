e512266d069bba5be55bf0d78a3285c2281de6b3
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-11 13:12:37 -0400

    add customers serializer :heavy_plus_sign:

--- user/serializers.py
+++ user/serializers.py
@@ -1,26 +1,32 @@
 from .models import Store, AppReviews
-from django.db.models import Count, Avg, Value, Subquery, OuterRef
+from django.db.models import Count, Sum, Avg, Value, Subquery, OuterRef
+from django.contrib.auth import get_user_model
 from product.serializers import ProductSerializer
 from rest_framework import serializers
 from django.db.models.functions import Coalesce
 from order.models import Order
+from product.models import Review
+
+USER = get_user_model()
 
 
 class StoreSerializer:
     def get_top_stores(self):
-        subquery_order = Order.objects.filter(status="DELEVRED").filter(id=OuterRef('id')).annotate(orders_count=Count('id')).values('orders_count')[:1]
+        subquery_order = Order.objects.filter(status="DELEVRED").filter(
+            id=OuterRef('id')).annotate(orders_count=Count('id')).values('orders_count')[:1]
         stores = Store.objects.annotate(order_count=Subquery(subquery_order), reviews_avg=Coalesce(Avg('product__review__stars'), Value(0.0))
-            ).order_by('-order_count', "-reviews_avg"
-            ).values(
-                'id', 'name', 'logo', 'order_count',
-                'description', 'reviews_avg'
-            )[:8]
+                                        ).order_by('-order_count', "-reviews_avg"
+                                                   ).values(
+            'id', 'name', 'logo', 'order_count',
+            'description', 'reviews_avg'
+        )[:8]
         return list(stores)
 
     def get_store_details(self, id):
-        subquery_order = Order.objects.filter(product__store__id=OuterRef('id'), status="DELEVRED").annotate(orders_count=Count('id')).values('orders_count')[:1]
+        subquery_order = Order.objects.filter(product__store__id=OuterRef(
+            'id'), status="DELEVRED").annotate(orders_count=Count('id')).values('orders_count')[:1]
         store = Store.objects.filter(pk=id).annotate(
-            orders=Coalesce(Subquery(subquery_order),0),
+            orders=Coalesce(Subquery(subquery_order), 0),
             reviews=Count('product__review'),
             reviews_avg=Coalesce(Avg('product__review__stars'), Value(0.0))
         )
@@ -46,3 +52,27 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = AppReviews
         fields = '__all__'
+
+
+class CustomersSerializer:
+    def get_data(self, user_id):
+        order_subquery = Order.objects.filter(
+            product__store__user__id=user_id,
+            status="DELEVRED",
+            user_id=OuterRef('id')
+        ).annotate(
+            orders=Count('pk'),
+            spent=Sum('price')
+        ).values('orders')[:1]
+        reviews_subquery = Review.objects.filter(
+            user_id=OuterRef('id')).annotate(reviews=Count('pk')).values('reviews')[:1]
+
+        customers = USER.objects.annotate(
+            orders=Coalesce(
+                Subquery(order_subquery), 0),
+            reviews=Coalesce(Subquery(reviews_subquery), 0)
+        ).filter(orders__gt=0).values(
+            'first_name', 'last_name', 'image',
+            'orders', 'reviews'
+        )
+        return customers
