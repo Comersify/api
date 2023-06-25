3c3f35a7c9a80344f395e4a868352d740ffff7e8
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-25 03:46:49 -0400

    fix make profile image func :lady_beetle:

--- user/models.py
+++ user/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import os
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 def make_profile_image_path(instance, filename):
     username = instance.username
     filename = f"profile_{filename}"
-    return os.path.join(f'uploads/{instance.type}', username, filename)
+    return os.path.join(f'uploads/{instance.user_type}', username, filename)
 
 
 def make_logo_image_path(instance, filename):
@@ -39,14 +40,26 @@ class CustomUser(AbstractUser):
         choices=TypeChoices.choices, default=TypeChoices.CUSTOMER, max_length=10)
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    auth_provider = models.CharField(max_length=20, default='EMAIL')
-    image = models.ImageField(upload_to="uplaods/user", null=True, blank=True)
+    image = models.ImageField(
+        upload_to=make_profile_image_path, null=True, blank=True)
     REQUIRED_FIELDS = ['email', 'first_name',
                        'last_name']
 
     def save(self, *args, **kwargs):
         self.username = self.email.split("@")[0]
         super().save(*args, **kwargs)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        response_data = {
+            "type": "success",
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'exp': refresh.payload['exp'],
+            "name": self.first_name,
+            "image": self.image.url
+        }
+        return response_data
 
 
 class ShippingInfo(models.Model):
