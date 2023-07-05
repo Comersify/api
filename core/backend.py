97d2ef18790ee41a7e3e1bd0e9c5cd5fbbbe48b2
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-04 22:58:35 -0400

    :heavy_plus_sign: add user token auth

--- core/backend.py
+++ core/backend.py
@@ -1,6 +1,7 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken
 from django.contrib.auth import get_user_model
+from user.models import Token
 
 User = get_user_model()
 
@@ -13,6 +14,23 @@ class AccessTokenBackend(JWTAuthentication):
             user = User.objects.filter(id=decoded_token['user_id']).get()
             request.user = user
         except InvalidToken:
+            return None
+
+        return (user, token)
+
+
+class UserTokenBackend:
+
+    def authenticate_header(self,request):
+        if 'X-Comercify-Individual-Seller' in request.headers:
+            return True
+        return False
+    def authenticate(self, request):
+        try:
+            token = request.headers['X-Comercify-Individual-Seller']
+            user = Token.objects.filter(token=token).get()
+            request.owner = user
+        except:
             return None
 
         return (user, token)
