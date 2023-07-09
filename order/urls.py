b7ecd98ba6fc1b0c188b6566f41b71678504a49c
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-09 18:20:50 -0400

    :heavy_plus_sign:  add orders analytics url

--- order/urls.py
+++ order/urls.py
@@ -3,8 +3,11 @@ from .views import *
 
 
 urlpatterns = [
-    path("my-orders/", GetMyOrdersView.as_view()),  
+    path("my-orders/", GetMyOrdersView.as_view()),
     path("order/create/", CreateOrderView.as_view()),
-    path("individual-seller/order/create/", CreateOrderForIndividualSeller.as_view()),
-    path("vendor/orders/", VendorOrdersView.as_view())
+    path("individual-seller/order/create/",
+         CreateOrderForIndividualSeller.as_view()),
+    path("vendor/orders/", VendorOrdersView.as_view()),
+    path("vendor/analytics/orders/", GetOrdersForLineChart.as_view())
+
 ]
