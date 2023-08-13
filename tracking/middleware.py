2a23f72550a677e7b13b1b38333088594fe6a543
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-08-13 02:27:58 -0400

    :heavy_minus_sign: delete debug line

--- tracking/middleware.py
+++ tracking/middleware.py
@@ -18,8 +18,6 @@ class TrackerMiddleware:
 
         client_url = request.META.get('HTTP_ORIGIN')
         api_path = request.META.get('PATH_INFO')
-        print("###########")
-        print(trackID)
         if trackID:
             Visit.objects.create(
                 tracker_id=trackID,
