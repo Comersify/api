4f291517d189a38ec5569a8df798c0185c03191d
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-02-05 01:22:09 +0100

    add test domain

--- website/models.py
+++ website/models.py
@@ -6,5 +6,6 @@ User = get_user_model()
 class Website(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={"user_type__in":[User.TypeChoices.INDIVIDUAL_SELLER, User.TypeChoices.STORE_OWNER]})
     domain = models.CharField(max_length=200, unique=True)
+    test_domain = models.CharField(max_length=200, unique=True)
     logo = models.ImageField(upload_to='logos/', null=True, blank=True)
     title = models.CharField(max_length=100)
