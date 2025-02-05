4bfe1c222719cc2a6faec4b49fdd789afa41f80d
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-02-05 01:16:54 +0100

    update website model

--- website/models.py
+++ website/models.py
@@ -5,6 +5,6 @@ User = get_user_model()
 
 class Website(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={"user_type__in":[User.TypeChoices.INDIVIDUAL_SELLER, User.TypeChoices.STORE_OWNER]})
-    domain = models.CharField(max_length=20, unique=True)
-    logo = models.ImageField()
-    title = models.CharField(max_length=20)
+    domain = models.CharField(max_length=200, unique=True)
+    logo = models.ImageField(upload_to='/logos', null=True, blank=True)
+    title = models.CharField(max_length=100)
